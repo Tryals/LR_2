44350959aa6c1d54e7ded075dbf25ca23dbe445c
1 <1@LAPTOP-2AF526SI>
2023-04-05 16:19:44 +0300

    Добавьте файлы проекта.

---
+++ LR_2/ClassAero.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR_2
+{
+    internal class ClassAero
+    {
+        //поля
+        private string name;
+        private double number;
+        private string type;
+        // свойства для доступа к полям
+        public string NAME
+        {
+            get { return name; }//возвращает значение поля
+            set { name = value; } //устанавливает значение поля
+        }
+
+        public double Num
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        public string TYPE
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        //методы 
+        //конструктор без параметров задает значения полей по умолчанию
+        public ClassAero()
+        {
+            name = "";
+            number = 0;
+            type = "";
+        }
+        //конструктор с параметрами
+        public ClassAero(string f, double d, string s)
+        {
+            name = f;
+            number = d;
+            type = s;
+        }
+        //метод вывода на экран
+        public string PrintToScreen()
+        {
+            string line = $"Пункт назначения {name} рейс № {number}" + "\n";
+            line += $"Тип самолёта {type}" + "\n";
+
+            return line;
+        }
+        public void SaveToFile(string filename)
+        {
+            StreamWriter streamWriter = new StreamWriter(filename, true, Encoding.Default);
+            streamWriter.WriteLine("Рейс");
+            streamWriter.WriteLine($"Место назначения: {name}");
+            streamWriter.WriteLine($"Номер: {number.ToString()}");
+            streamWriter.WriteLine($"Самолёт: {type}");
+            streamWriter.Close();
+        }
+
+
+    }
+}
---
+++ LR_2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LR_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClassAero aero = new ClassAero();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSaveaero_Click(object sender, RoutedEventArgs e)
+        {
+            aero.NAME = TxtN.Text;
+            aero.Num = double.Parse(TxtN2.Text);
+            aero.TYPE = TxtStip.Text;
+
+            LstAero.Items.Clear();
+            LstAero.Items.Add(aero.PrintToScreen());
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == false)
+                return;
+            aero.SaveToFile(saveFileDialog.FileName);
+        }
+    }
+}
